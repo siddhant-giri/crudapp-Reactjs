8c95fdcd573f9196d476b61a717569d4991f41f7
sidgiri55555 <sidgiri2000@gmail.com>
2022-04-04 11:58:06 +0530

    first commit

---
+++ src/utils/config.js
@@ -0,0 +1,20 @@
+//TODO: DONE add firebase configuration and export it
+export const firebaseConfig = {
+  //TODO: ADD YOUR FIREBASE INFO HERE
+
+    apiKey: "AIzaSyAy1814uT2-3_53rJK7VTHgMdm7x9XoV2U",
+    authDomain: "mygitapp-fe2f9.firebaseapp.com",
+    databaseURL: "https://mygitapp-fe2f9-default-rtdb.firebaseio.com",
+    projectId: "mygitapp-fe2f9",
+    storageBucket: "mygitapp-fe2f9.appspot.com",
+    messagingSenderId: "310514304085",
+    appId: "1:310514304085:web:4778aadbf9b36e185200dc"
+
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
